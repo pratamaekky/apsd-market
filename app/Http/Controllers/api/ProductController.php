ab06a3c3765bf2640e129b4f595b3cc8eec827fc
pratamaekky <50559449+pratamaekky@users.noreply.github.com>
2023-06-02 23:10:38 +0700

    Next

---
+++ app/Http/Controllers/api/ProductController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Product;
+
+use App\Http\Controllers\TransformerController;
+
+class ProductController extends Controller
+{
+    //
+    public $productData;
+
+    public function __construct()
+    {
+        $this->product = new Product;
+    }
+    public function index()
+    {
+        $productData = $this->product->store();
+
+        $resData = [];
+
+        // dd($productData);
+        foreach ($productData as $product) {
+            $resData[] = [
+                "name" => $product->name,
+                "type" => TransformerController::convertProductType($product->type),
+                "description" => $product->description,
+                "price" => intval($product->price),
+                "stock" => $product->stock
+            ];
+        }
+
+        return $resData;
+        // dd($product);
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,19 @@ class Product extends Model
     use HasFactory;
     protected $table = 'tbl_product';
     protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'type',
+        'description',
+        'price',
+        'stock',
+        'status'
+    ];
+
+    public function store()
+    {
+        $product = $this->all();
+
+        return $product;
+    }
 }
